afec3da18061f0ff160e0e533eeae22c5800480b
akash8663 <akash8663@gmail.com>
2026-06-25 10:39:08 -0400

    feat: add accessibility tests and update visual test snapshot handling

---
+++ test-cases/accessibility.spec.ts
@@ -0,0 +1,21 @@
+
+import AxeBuilder from '@axe-core/playwright';
+import { expect, test } from 'fixtures';
+
+test.describe('homepage', () => {
+  test('should not have any automatically detectable accessibility issues', async ({ homePage, page }) => {
+
+    const accessibilityScanResults = await new AxeBuilder({ page })
+        .disableRules(['color-contrast'])
+        .analyze();
+
+    // expect(accessibilityScanResults.violations).toEqual([]);
+
+    const criticalOrSerious = accessibilityScanResults.violations.filter(
+      v => v.impact === 'critical' || v.impact === 'serious'
+    );
+
+    expect(criticalOrSerious).toHaveLength(0);
+
+  });
+});
--- test-cases/visual.spec.ts
+++ test-cases/visual.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "fixtures";
 
 test('Visual test', async ({ homePage, page }) => {
-    await expect(page).toHaveScreenshot({fullPage: true, animations: 'disabled'});
+    await expect(page).toHaveScreenshot('homepage.png', {fullPage: true, animations: 'disabled'});
 });
