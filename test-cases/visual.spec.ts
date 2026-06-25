b34f30202a95270b8d9dafe4b5ae544e368aad5b
akash8663 <akash8663@gmail.com>
2026-06-25 14:29:28 -0400

    test: skip visual test for future implementation

--- test-cases/visual.spec.ts
+++ test-cases/visual.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "fixtures";
 
-test('Visual test', async ({ homePage, page }) => {
+test.skip('Visual test', async ({ homePage, page }) => {
     await expect(page).toHaveScreenshot('homepage.png', {fullPage: true, animations: 'disabled'});
 });
